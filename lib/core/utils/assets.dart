8540cd8c0e4fd595a9c51342653e7bdc401e8948
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-21 14:50:19 +0200

    create assets file for images

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesQrCodeImage
+  /// assets/images/qr_code_image.svg
+  static const String assetsImagesQrCodeImage = "assets/images/qr_code_image.svg";
+
+  /// Assets for assetsImagesShoppingImage
+  /// assets/images/shopping_image.svg
+  static const String assetsImagesShoppingImage = "assets/images/shopping_image.svg";
+
+  /// Assets for assetsImagesSplashImage
+  /// assets/images/splash_image.svg
+  static const String assetsImagesSplashImage = "assets/images/splash_image.svg";
+}
+
