673412efb426439b5c0de820048b2b6f46d568c7
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-21 14:55:03 +0200

    add styles utility class with text styles

---
+++ lib/core/utils/styels.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+abstract class Styels {
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+}
