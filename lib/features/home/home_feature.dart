31fde4144df8b0d34b1730a1d3b9e4dca5ea26ba
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-27 21:26:25 +0200

    create settings page and add route to home feature

--- lib/features/home/home_feature.dart
+++ lib/features/home/home_feature.dart
@@ -1,4 +1,5 @@
 import 'package:app_features/app_features.dart';
+import 'package:tasky/features/home/pages/settings_page.dart';
 import 'bloc/home_bloc.dart';
 import '../../config/app_config.dart';
 import 'home_page.dart';
@@ -8,11 +9,17 @@ class HomeFeature extends Feature {
   void get dependencies => {getIt.registerLazySingleton(() => HomeBloc())};
   @override
   String get name => '/home';
+  String get settingPage => '/settings';
   static HomeFeature get to => AppFeatures.get();
   @override
   List<GoRoute> get routes => [
     GoRoute(path: name, name: name, builder: (_, state) => const HomePage()),
+    GoRoute(
+      path: settingPage,
+      name: settingPage,
+      builder: (_, state) => const SettingsPage(),
+    ),
   ];
 
-  void goToHomePage() => push(name: name);
+  void goToSettingsPage() => push(name: settingPage);
 }
