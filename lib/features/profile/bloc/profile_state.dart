9e905777d5237480b22928e8f51ae35911b5e83c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-27 17:09:38 +0200

    implemnt code in profiel state file

--- lib/features/profile/bloc/profile_state.dart
+++ lib/features/profile/bloc/profile_state.dart
@@ -1,9 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:tasky/app/models/profile_model.dart';
 
 class ProfileState extends Equatable {
-  const ProfileState();
+  const ProfileState({this.isloading, this.errorMessage, this.profileModel});
+  final bool? isloading;
+  final String? errorMessage;
+  final ProfileModel? profileModel;
 
-  ProfileState copyWith() => ProfileState();
+  ProfileState copyWith({
+    bool? isloading,
+    String? errorMessage,
+    ProfileModel? profileModel,
+  }) => ProfileState(
+    isloading: isloading ?? this.isloading,
+    errorMessage: errorMessage ?? this.errorMessage,
+    profileModel: profileModel ?? this.profileModel,
+  );
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [isloading, errorMessage, profileModel];
 }
