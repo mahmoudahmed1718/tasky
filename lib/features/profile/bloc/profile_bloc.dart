5b1180d20e35279e2e35a4f36a26c042f0ae638b
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-27 20:21:35 +0200

    feat: implement getProfile method in ProfileBloc for fetching user profile data

--- lib/features/profile/bloc/profile_bloc.dart
+++ lib/features/profile/bloc/profile_bloc.dart
@@ -1,6 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tasky/app/models/profile_model.dart';
+import 'package:tasky/app/utils/notification_util.dart';
+import 'package:tasky/config/app_config.dart';
+import 'package:tasky/core/app_storage.dart';
+import 'package:tasky/features/profile/action/profile_action.dart';
 import 'package:tasky/features/profile/bloc/profile_state.dart';
 
 class ProfileBloc extends Cubit<ProfileState> {
   ProfileBloc() : super(ProfileState());
+  static ProfileBloc to = getIt.get();
+
+  Future<void> getProfile() async {
+    await ProfileAction()
+        .listen(
+          onStart: () => NotificationUtil.showLoading(),
+          onDone: () => NotificationUtil.hideLoading(),
+          onSuccess: (response) {
+            emit(state.copyWith(profileModel: ProfileModel()));
+          },
+          onError: (error) {
+            NotificationUtil.showError(error.message);
+          },
+        )
+        .withHeader("authorization", AppStorage.to.getToken())
+        .execute();
+  }
 }
