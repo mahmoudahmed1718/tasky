01282485a1751da7d7768331d8c734f34caccc2c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-29 14:37:09 +0200

    create task model

---
+++ lib/app/models/task_model.dart
@@ -0,0 +1,38 @@
+class TaskModel {
+  String? id;
+  String? image;
+  String? title;
+  String? desc;
+  String? priority;
+  String? status;
+  String? user;
+  String? createdAt;
+  String? updatedAt;
+  int? version;
+
+  TaskModel({
+    this.id,
+    this.image,
+    this.title,
+    this.desc,
+    this.priority,
+    this.status,
+    this.user,
+    this.createdAt,
+    this.updatedAt,
+    this.version,
+  });
+
+  // TaskModel.fromJson(TaskResponse json) {
+  //   id = json.id;
+  //   image = json.image;
+  //   title = json.title;
+  //   desc = json.desc;
+  //   priority = json.priority;
+  //   status = json.status;
+  //   user = json.user;
+  //   createdAt = json.createdAt;
+  //   updatedAt = json.updatedAt;
+  //   version = json.version;
+  // }
+}
