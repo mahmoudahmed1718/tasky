209546a667e7d749a77c93ce28ea1ff1bb9c45c8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-01-29 21:28:58 +0200

    feat: enhance CreateNewTaskPage layout and add form validation

--- lib/features/tasks/pages/create_new_task_page.dart
+++ lib/features/tasks/pages/create_new_task_page.dart
@@ -1,6 +1,7 @@
 import 'package:app_forms/app_forms.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:gap/gap.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tasky/core/extensions/context_extension.dart';
@@ -19,55 +20,96 @@ class CreateNewTaskPage extends StatelessWidget {
         leading: Arrowbackwidget(),
       ),
 
-      body: Column(
-        children: [
-          DottedBorder(
-            child: InkWell(
-              onTap: () async {
-                final image = await PickImageAction().pickImage(
-                  ImageSource.gallery,
-                );
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            DottedBorder(
+              child: InkWell(
+                onTap: () async {
+                  final image = await PickImageAction().pickImage(
+                    ImageSource.gallery,
+                  );
 
-                if (image != null) {
-                  final String imagePath = image.path;
-                  print("Selected Image Path: $imagePath");
-                }
-              },
-              borderRadius: BorderRadius.circular(12),
-              child: Container(
-                height: 54, // Adjust based on your needs
-                width: context.width,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.add_photo_alternate_outlined,
-                      color: AppColors.primaryColor,
-                      size: 28,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "Add Img",
-                      style: TextStyle(
-                        color: Colors.deepPurple,
-                        fontSize: 22,
-                        fontWeight: FontWeight.w500,
+                  if (image != null) {
+                    final String imagePath = image.path;
+                    print("Selected Image Path: $imagePath");
+                  }
+                },
+
+                child: Container(
+                  height: 54, // Adjust based on your needs
+                  width: context.width,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.add_photo_alternate_outlined,
+                        color: AppColors.primaryColor,
+                        size: 28,
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 10),
+                      Text(
+                        "Add Img",
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Gap(8),
-          FormBuilder(
-            child: Column(children: [Text("Title", style: Styels.textStyle16)]),
-          ),
-        ],
+            Gap(12),
+            FormBuilder(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Task Title", style: Styels.textStyle14),
+                  Gap(6),
+                  FormBuilderTextField(
+                    name: "task title",
+                    onSaved: (newValue) {},
+                    validator: FormBuilderValidators.required(
+                      errorText: "Task title is required",
+                    ),
+                    decoration: InputDecoration(
+                      hint: Text("Task title", style: Styels.textStyle14),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                  Gap(14),
+                  Text("Task Description", style: Styels.textStyle14),
+                  Gap(6),
+                  FormBuilderTextField(
+                    name: "task title",
+                    maxLines: 6,
+                    onSaved: (newValue) {},
+                    decoration: InputDecoration(
+                      hint: Text(
+                        "Enter Task Description",
+                        style: Styels.textStyle14,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
